4fe0ec0d7731eba054af2b2f31bf1c96773dc116
Anurag <hamsen3880@gmail.com>
2024-08-02 21:54:29 +0530

    Hero Section V0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Herosection from "@/components/HeroSection"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center"></h1>
+      <Herosection />
     </main>
   );
 }
